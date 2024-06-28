81f125fc9ba0ce0fb385042ee160ced5090f9d38
PETER MARQUIE <38792945+pmarquie@users.noreply.github.com>
2024-06-28 10:25:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
 )
-import streamlit as st
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The Name on your Smoothie will be:', name_on_order)
@@ -36,7 +35,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + " "
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', SEARCH_ON, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
